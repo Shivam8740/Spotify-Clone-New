825368817c2ef981ee58786c80b88db3883d8740
Shivam8740 <120658151+Shivam8740@users.noreply.github.com>
2023-06-29 20:53:30 +0530

    Add files via upload

---
+++ js/music-song.js
@@ -0,0 +1,122 @@
+let allMusic = [
+    {
+        name: "Astronaut in the Ocean",
+        artist:"Untitled",
+        img:"Astronaut in the Ocean",
+        src:"Astronaut in the Ocean"
+    },
+    {
+        name: "Aarambh hai Prachand",
+        artist:"Untitled",
+        img:"Aarambh hai Prachand",
+        src:"Aarambh hai Prachand"
+    },
+    {
+        name: "Bruno Mars - 24K Magic",
+        artist:"Untitled",
+        img:"Bruno Mars - 24K Magic",
+        src:"Bruno Mars - 24K Magic"
+    },
+    {
+        name: "Eminem - Till I Collapse",
+        artist:"Untitled",
+        img:"Eminem - Till I Collapse",
+        src:"Eminem - Till I Collapse"
+    },
+    {
+        name: "FIFTY FIFTY  - Cupid",
+        artist:"Untitled",
+        img:"FIFTY FIFTY  - Cupid",
+        src:"FIFTY FIFTY  - Cupid"
+    },
+    {
+        name: "FREDDIE DREDD - LIMBO",
+        artist:"Untitled",
+        img:"FREDDIE DREDD - LIMBO",
+        src:"FREDDIE DREDD - LIMBO"
+    },
+    {
+        name: "Giga Chad theme",
+        artist:"Untitled",
+        img:"Giga Chad theme",
+        src:"Giga Chad theme"
+    },
+    {
+        name: "HENSONN - SAHARA",
+        artist:"Untitled",
+        img:"HENSONN - SAHARA",
+        src:"HENSONN - SAHARA"
+    },
+    {
+        name: "Imagine Dragons - Believer",
+        artist:"Untitled",
+        img:"Imagine Dragons - Believer",
+        src:"Imagine Dragons - Believer"
+    },
+    {
+        name: "METAMORPHOSIS",
+        artist:"Untitled",
+        img:"METAMORPHOSIS",
+        src:"METAMORPHOSIS"
+    },
+    {
+        name: "MURDER IN MY MIND",
+        artist:"Untitled",
+        img:"MURDER IN MY MIND",
+        src:"MURDER IN MY MIND"
+    },
+    {
+        name: "Safar",
+        artist:"Untitled",
+        img:"Safar",
+        src:"Safar"
+    },
+    {
+        name: "SHOORVEER 3",
+        artist:"Untitled",
+        img:"SHOORVEER 3",
+        src:"SHOORVEER 3"
+    },
+    {
+        name: "SHOORVEER",
+        artist:"Untitled",
+        img:"SHOORVEER",
+        src:"SHOORVEER"
+    },
+    {
+        name: "Sober",
+        artist:"Untitled",
+        img:"Sober",
+        src:"Sober"
+    },
+    {
+        name: "Soulja Boy - Kiss Me Thru The Phone",
+        artist:"Untitled",
+        img:"Soulja Boy - Kiss Me Thru The Phone",
+        src:"Soulja Boy - Kiss Me Thru The Phone"
+    },
+    {
+        name: "Tech N9ne",
+        artist:"Untitled",
+        img:"Tech N9ne",
+        src:"Tech N9ne"
+    },
+    {
+        name: "The Local Train - Choo Lo",
+        artist:"Untitled",
+        img:"The Local Train - Choo Lo",
+        src:"The Local Train - Choo Lo"
+    },
+    {
+        name: "The Tututu",
+        artist:"Untitled",
+        img:"The Tututu",
+        src:"The Tututu"
+    },
+    {
+        name: "Xcho",
+        artist:"Untitled",
+        img:"Xcho",
+        src:"Xcho"
+    }
+];
